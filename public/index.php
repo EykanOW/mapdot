3832de53c7c41eadc7d59b2f4ab45794d16f6e99
Erik <kallgrenerik@gmail.com>
2024-06-10 02:47:47 +0200

    more functionality, remove dots, reset circle position, distance to dots and markers, lines pointing towards markers

--- public/index.php
+++ public/index.php
@@ -11,11 +11,12 @@
     <title>Mapdot</title>
 </head>
 <body>
-    <div id="map" style="height: 600;"></div>
+    <div id="map" style="height: 800;"></div>
 
         <input type="number" name="radius" min="0" id="radius" onchange="setCircle()" placeholder="Diameter in kilometers">
         <input type="button" value="Randomize point" onclick="getRandomPoint()">
         <input type="button" value="Remove dots" onclick="removeDots()">
+        <input type="button" value="Reset Circle" onclick="resetCircle()">
 
 
     <script src="https://unpkg.com/leaflet/dist/leaflet.js"></script>
@@ -30,19 +31,42 @@
         attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
         }).addTo(map);
 
-        var circle = L.circle([61.30434,17.05966], {
+        // my code
+
+        var dots = [];
+        var lines = [];
+        const area = {
+            lat:61.30434,
+            lng:17.05966,
+            rad:0
+        }
+        const initialPosition = [61.30434, 17.05966];
+
+        var circle = L.circle([area.lat,area.lng], {
             color: 'green',
             fillColor: 'green',
             fillOpacity: 0.2,
             radius: 0
         }).addTo(map);
 
-        var dots = [];
-        const area = {
-            lat:61.30434,
-            lng:17.05966,
-            rad:0
+        function resetCircle(){
+            circle.setLatLng(initialPosition);
+            area.lat = initialPosition[0];
+            area.lng = initialPosition[1];
         }
+
+        function onMapClick(e) {
+
+            area.lat = e.latlng.lat;
+            area.lng = e.latlng.lng;
+            circle.setLatLng(e.latlng);
+            // popup
+            // .setLatLng(e.latlng)
+            // .setContent("You clicked the map at " + e.latlng.toString())
+            // .openOn(map);
+        }
+
+
 
         var diameter = document.getElementById("radius");
 
@@ -51,9 +75,9 @@
             area.rad = diameter.value * 1000/2;
             circle.setRadius(area.rad);
 
-            let currentUrl = new URL(window.location.href);
-            currentUrl.searchParams.set('radius', area.rad);
-            window.history.pushState({}, '', currentUrl);
+            // let currentUrl = new URL(window.location.href);
+            // currentUrl.searchParams.set('radius', area.rad);
+            // window.history.pushState({}, '', currentUrl);
         }
 
         function getRandomPoint() {
@@ -68,18 +92,30 @@
                 color: 'red',
                 fillColor: 'red',
                 fillOpacity: 0.2,
-                radius: 50
+                radius: 30
             }).addTo(map);
             dots.push(dot);
+            dot.bindPopup(`<b>Travel here!</b><br>${Math.round(distance)/1000}km away from center.`).openPopup();
+            var line = L.polygon([
+                [area.lat, area.lng],
+                [foundLatitude, foundLongitude]
+            ]).addTo(map);
+            lines.push(line);
         }
 
 
         function removeDots() {
             for (var i = 0; i < dots.length; i++) {
                 map.removeLayer(dots[i]);
+                map.removeLayer(lines[i]);
             }
             dots = [];  // Clear the array
+            lines = []
         }
+
+        var popup = L.popup();
+
+        map.on('click', onMapClick);
 
     </script>
 </body>
